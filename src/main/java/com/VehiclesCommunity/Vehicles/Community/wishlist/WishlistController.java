1ba418da2d4be46d4106ad361f9fafaf4568174a
Bassem <bassemsaeed620@gmail.com>
2024-05-07 22:35:27 +0300

    Wishlist updated

--- src/main/java/com/VehiclesCommunity/Vehicles/Community/wishlist/WishlistController.java
+++ src/main/java/com/VehiclesCommunity/Vehicles/Community/wishlist/WishlistController.java
@@ -1,6 +1,8 @@
 package com.VehiclesCommunity.Vehicles.Community.wishlist;
 
+import com.VehiclesCommunity.Vehicles.Community.user.User;
 import com.VehiclesCommunity.Vehicles.Community.vehicle.Vehicle;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,20 +18,23 @@ public class WishlistController {
         this.wishlistService = wishlistService;
     }
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<Map<String, List<Vehicle>>> getVehicle(@PathVariable Integer userId) {
-        return ResponseEntity.ok(Map.of("data", wishlistService.getUserWishlist(userId)));
+    @GetMapping("/all")
+    public ResponseEntity<Map<String, List<Vehicle>>> getUserWishlist(HttpServletRequest request) {
+        User userDetails = (User) request.getAttribute("userDetails");
+        return ResponseEntity.ok(Map.of("data", wishlistService.getUserWishlist(userDetails.getId())));
     }
 
-    @PostMapping("/like/{userId}/{vehicleId}")
-    public ResponseEntity<Map<String, String>> addToWishlist(@PathVariable Integer userId, @PathVariable Integer vehicleId) {
-        wishlistService.addToWishlist(userId, vehicleId);
+    @PostMapping("/like/{vehicleId}")
+    public ResponseEntity<Map<String, String>> addToWishlist(HttpServletRequest request, @PathVariable Integer vehicleId) {
+        User userDetails = (User) request.getAttribute("userDetails");
+        wishlistService.addToWishlist(userDetails.getId(), vehicleId);
         return ResponseEntity.ok(Map.of("message", "Vehicle added to wishlist successfully"));
     }
 
-    @PostMapping("/unlike/{userId}/{vehicleId}")
-    public ResponseEntity<Map<String, String>> removeFromVehicle(@PathVariable Integer userId, @PathVariable Integer vehicleId) {
-        wishlistService.removeFromWishlist(userId, vehicleId);
+    @PostMapping("/unlike/{vehicleId}")
+    public ResponseEntity<Map<String, String>> removeFromVehicle(HttpServletRequest request, @PathVariable Integer vehicleId) {
+        User userDetails = (User) request.getAttribute("userDetails");
+        wishlistService.removeFromWishlist(userDetails.getId(), vehicleId);
         return ResponseEntity.ok(Map.of("message", "Vehicle removed from wishlist successfully"));
     }
 }
